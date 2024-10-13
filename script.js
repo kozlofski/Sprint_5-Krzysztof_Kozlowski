6f4848f1fa94f504f0c23ca520843a6096377ab5
Krzysztof Kozlowski <kozlofski.dev@gmail.com>
2024-10-13 14:45:13 +0200

    Calculating age - complete function

--- script.js
+++ script.js
@@ -1,78 +1,103 @@
 const inputData = [
-    {
-      firstName: false,
-      lastName: 2,
-    },
-    {
-      firstName: "Roman",
-      lastName: "Kowalski",
-    },
-  
-    {
-      firstName: "Halina",
-      lastName: "Malina",
-    },
-    {
-      firstName: "B",
-      lastName: "22",
-    },
-    {
-      firstName: "Jan",
-      lastName: "Nowak",
-    },
-    {
-      firstName: "Kamil",
-      lastName: null,
-    },
-  ];
-  
+  {
+    firstName: false,
+    lastName: 2,
+  },
+  {
+    firstName: "Roman",
+    lastName: "Kowalski",
+  },
+  {
+    firstName: "Halina",
+    lastName: "Malina",
+  },
+  {
+    firstName: "B",
+    lastName: "22",
+  },
+  {
+    firstName: "Jan",
+    lastName: "Nowak",
+  },
+  {
+    firstName: "Kamil",
+    lastName: null,
+  },
+];
+
 // TASK 01
 
 const userNameIsProper = (user) => {
-return typeof user.firstName === 'string' &&
-typeof user.lastName === 'string' &&
-user.firstName.length >= 3 &&
-user.lastName.length >= 3;
+  return (
+    typeof user.firstName === "string" &&
+    typeof user.lastName === "string" &&
+    user.firstName.length >= 3 &&
+    user.lastName.length >= 3
+  );
 };
 
-const capitalizeFirstLetter = inputString => {
-if(!inputString) {
-    return '';
-}
-return `${inputString[0].toUpperCase()}${inputString.slice(1)}`
+const capitalizeFirstLetter = (inputString) => {
+  if (!inputString) {
+    return "";
+  }
+  return `${inputString[0].toUpperCase()}${inputString.slice(1)}`;
 };
 
 const createNick = (user) => {
-const firstHalf = user.firstName.slice(-3).toLowerCase().split('').reverse().join('');
-const secondHalf = user.lastName.slice(0, 3).toLowerCase().split('').reverse().join('');
-const nickLowCase = `${firstHalf}${secondHalf}`;
-return capitalizeFirstLetter(nickLowCase);
+  const firstHalf = user.firstName
+    .slice(-3)
+    .toLowerCase()
+    .split("")
+    .reverse()
+    .join("");
+  const secondHalf = user.lastName
+    .slice(0, 3)
+    .toLowerCase()
+    .split("")
+    .reverse()
+    .join("");
+  const nickLowCase = `${firstHalf}${secondHalf}`;
+  return capitalizeFirstLetter(nickLowCase);
 };
 
 const addNickName = (user) => {
-    if(userNameIsProper(user)) {
-        user.nickname = createNick(user);
-    }
-}
-    
+  if (userNameIsProper(user)) {
+    user.nickname = createNick(user);
+  }
+};
+
 inputData.forEach(addNickName);
-    
+
 // TASK 02
 
 const hasNickName = (user) => {
-    return 'nickname' in user;
+  return "nickname" in user;
 };
 
-const calculateAge = (user) => {
-    const numberOfLetters = user.firstName.length + user.lastName.length;
-    return age = numberOfLetters;
+const processKeys = (user, index) => {
+  const divisor = index === 0 ? 1 : index;
+  const userKeys = Object.keys(user);
+  const totalLetters = userKeys.reduce((sum, keyName) => {
+    return sum + keyName.length;
+  }, 0);
+
+  return Math.ceil(totalLetters / divisor);
 };
 
-const addAge = (user) => {
-    if(hasNickName(user)) {
-        const age = calculateAge(user);
-        user.age = age;
-    }
+const calculateAge = (user, index) => {
+  const numberOfLettersInName = user.firstName.length + user.lastName.length;
+  const age =
+    numberOfLettersInName % 2 === 0
+      ? numberOfLettersInName
+      : processKeys(user, index);
+  return age;
+};
+
+const addAge = (user, index) => {
+  if (hasNickName(user)) {
+    const age = calculateAge(user, index);
+    user.age = age;
+  }
 };
 
 inputData.forEach(addAge);
